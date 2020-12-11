44790f5903864931b195ac9eec54d706123ec383
Jerwin Wieser <j2.wieser@gmail.com>
2020-12-12 00:29:27 +0100

    clean repo

--- createletters.R
+++ createletters.R
@@ -3,9 +3,7 @@ library(dplyr)
 library(purrr)
 
 unlink("output", recursive=T)
-unlink("pdf", recursive=T)
 dir.create("output")
-dir.create("pdf")
 
 # read csv file -----------------------------------------------------------
 df <- read.csv2("details.csv", stringsAsFactors = F)
@@ -15,15 +13,21 @@ create_dirs <- function(x=df) {
   dir <- paste0("output/", x[["companynameshort"]])
   letterdate <- gsub("/", "-", x[["letterdate"]])
   filenamenew <- paste0(dir,"/", x[["companynameshort"]], "_letter_", letterdate, ".tex")
+  bodynew <- paste0(dir,"/", "body.txt")
+  emailnew <- paste0(dir,"/", "email.txt")
   if (!file.exists(dir)){
     # if the file does not yet exist, create it 
     print(paste0('creating the dir ', dir))
     dir.create(dir)
     file.copy(from = "template/letter.tex", to = filenamenew, overwrite = T)
+    file.copy(from = "template/body.txt", to = bodynew, overwrite = T)
+    file.copy(from = "template/email.txt", to = emailnew, overwrite = T)
   } else {
     # if the file does exist, overwrite the existing one for now
     print(paste0('dir ', dir, 'already exists, but copying the directory anyway for now, poissbly overwriting the files in it..'))
     file.copy(from = "template/letter.tex", to = filenamenew, overwrite = T)
+    file.copy(from = "template/body.txt", to = bodynew, overwrite = T)
+    file.copy(from = "template/email.txt", to = emailnew, overwrite = T)
   }
 }
 
