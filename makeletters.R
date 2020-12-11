873cb6b9706aa99be86f809482d79e89ea66d6b6
Jerwin Wieser <j2.wieser@gmail.com>
2020-12-11 21:05:42 +0100

    some fixes

--- makeletters.R
+++ makeletters.R
@@ -1,28 +1,32 @@
+# create letter with latex ------------------------------------------------
+library(dplyr)
+library(purrr)
 
-details <- read.csv2("details.csv", stringsAsFactors = F)
-detailscase <- details[1,]
+# read csv file -----------------------------------------------------------
+df <- read.csv2("details.csv", stringsAsFactors = F)
 
-create_dirs <- function(x="lettertest/") {
-  x <- paste0("output/",x)
+# create dirs -------------------------------------------------------------
+create_dirs <- function(x=df) {
+  x <- paste0("output/",x[["companynameshort"]])
   if (!file.exists(x)){
+    # if the file does not yet exist, create it 
     print(paste0('creating the dir ', x))
     dir.create(x)
     file.copy(from = "template/letter.tex", to = paste0(x,"/letter.tex"), overwrite = T)
   } else {
+    # if the file does exist, overwrite the existing one for now
     print(paste0('dir ', x, 'already exists, but copying the directory anyway for now, poissbly overwriting the files in it..'))
     file.copy(from = "template/letter.tex", to = paste0(x,"/letter.tex"), overwrite = T)
   }
 }
 
-company <- detailscase$companynameshort
-create_dirs(x=company)
-
-compose_letter <- function(x) {
-  filename <- paste0("output/", x$companynameshort, "/letter.tex")
+# compose the letter ------------------------------------------------------
+compose_letter <- function(x=df) {
+  filename <- paste0("output/", x[["companynameshort"]], "/letter.tex")
   fileconnection <- file(filename)
   content <- readLines(filename)
   contentadj <- content
-  for (i in ncol(details)) {
+  for (i in c(1:ncol(details))) {
     var <- names(details)[i]
     pattern <- paste0('\\newcommand{\\', var, '}{', var, '}')
     replacement <- paste0("\\newcommand{\\", var, "}{", details[i], "}")
@@ -32,4 +36,16 @@ compose_letter <- function(x) {
   close(fileconnection)
 }
 
-compose_letter(x=detailscase)
+# apply functions ---------------------------------------------------------
+for (i in c(1,2)){
+  create_dirs(df[i,])
+  compose_letter(df[i,])
+}
+
+# compile pdfs ------------------------------------------------------------
+system('bash compile_latex_pdf.sh')
+
+# list pdf files ----------------------------------------------------------
+files <- list.files("output", full.names = T, recursive = T)
+filespdf <- grep(".pdf", files, value =T)
+
