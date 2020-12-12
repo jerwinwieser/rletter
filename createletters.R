c09f06c02e2807b70de5228f63951dee188daeea
Jerwin Wieser <j2.wieser@gmail.com>
2020-12-12 19:58:11 +0100

    last updates

--- createletters.R
+++ createletters.R
@@ -2,40 +2,28 @@
 library(dplyr)
 library(purrr)
 
-unlink("output", recursive=T)
-dir.create("output")
-
 # read csv file -----------------------------------------------------------
 df <- read.csv2("details.csv", stringsAsFactors = F)
 
-# create dirs -------------------------------------------------------------
-create_dirs <- function(x=df) {
-  dir <- paste0("output/", x[["companynameshort"]])
-  letterdate <- gsub("/", "-", x[["letterdate"]])
-  filenamenew <- paste0(dir,"/", x[["companynameshort"]], "_letter_", letterdate, ".tex")
-  bodynew <- paste0(dir,"/", "body.txt")
-  emailnew <- paste0(dir,"/", "email.txt")
-  if (!file.exists(dir)){
-    # if the file does not yet exist, create it 
-    print(paste0('creating the dir ', dir))
-    dir.create(dir)
-    file.copy(from = "template/letter.tex", to = filenamenew, overwrite = T)
-    file.copy(from = "template/body.txt", to = bodynew, overwrite = T)
-    file.copy(from = "template/email.txt", to = emailnew, overwrite = T)
-  } else {
-    # if the file does exist, overwrite the existing one for now
-    print(paste0('dir ', dir, 'already exists, but copying the directory anyway for now, poissbly overwriting the files in it..'))
-    file.copy(from = "template/letter.tex", to = filenamenew, overwrite = T)
-    file.copy(from = "template/body.txt", to = bodynew, overwrite = T)
-    file.copy(from = "template/email.txt", to = emailnew, overwrite = T)
-  }
+df <- df %>% 
+  filter(createletter ==1) %>% 
+  mutate(dir = paste0("output/", companynameshort, "_", gsub("/", "-", letterdate)))
+
+# copy files --------------------------------------------------------------
+copy_files <- function(x) {
+  file.copy(from = "template/letter.tex", to = x, overwrite = F)
+  file.copy(from = "template/body.txt", to = x, overwrite = F)
+  file.copy(from = "template/email.txt", to = x, overwrite = F)
 }
 
+unlink(df$dir, recursive=TRUE)
+map(df$dir, dir.create)
+map(df$dir, copy_files)
+
 # compose the letter ------------------------------------------------------
-compose_letter <- function(x=df) {
-  letterdate <- gsub("/", "-", x[["letterdate"]])
-  filename <- paste0("output/", x[["companynameshort"]], "/", x[["companynameshort"]], "_letter_", letterdate, ".tex")
-  print(filename)
+compose_letter <- function(x) {
+  print(x)
+  filename <- paste0(x$dir, "/letter.tex")
   fileconnection <- file(filename)
   content <- readLines(filename)
   contentadj <- content
@@ -49,17 +37,17 @@ compose_letter <- function(x=df) {
   close(fileconnection)
 }
 
-# apply functions ---------------------------------------------------------
-for (i in seq_along(df)){
-  create_dirs(df[i,])
-  compose_letter(df[i,])
+for (i in c(1:nrow(df))) {
+  print(i)
+  compose_letter(x = df[i,])
 }
 
 # compile pdfs ------------------------------------------------------------
-# system('bash compile_latex_pdf.sh')
+system('bash compile_latex_pdf.sh')
 
-# list pdf files ----------------------------------------------------------
-# files <- list.files("output", full.names = T, recursive = T, pattern = ".pdf")
-# file.copy(from = files, to = "pdf/", overwrite = T)
+# make backup -------------------------------------------------------------
+files <- list.files("output", full.names = T, recursive = T, pattern = ".pdf")
+filesbackup <- paste0("pdf/", basename(dirname(files)), "_", basename(files))
+map2(.x=files, .y=filesbackup, function(.x,.y) {file.copy(from=.x, to=.y, overwrite=T)})
 
           
